48f657ae6b70998c617708620d8eedd5bce0a981
Alex Zosyk <alekcandrzosyk@gmail.com>
2019-08-22 09:23:08 +0300

    created User model, and added to rest response call.

--- src/main/java/com/app/mobileappws/controller/UserController.java
+++ src/main/java/com/app/mobileappws/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.app.mobileappws.controller;
 
+import com.app.mobileappws.model.response.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,13 +9,20 @@ public class UserController {
 
 
     @GetMapping
-    public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page, @RequestParam("limit") int limit) {
-        return "get users was called with page = " + page + " and limit = " + limit;
+    public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
+                           @RequestParam(value = "limit", defaultValue = "50") int limit,
+                           @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
+        return "get users was called with page = " + page + " and limit = " + limit + " , and sort: " + sort;
     }
 
     @GetMapping(path = "/{userId}")
-    public String getUser(@PathVariable int userId) {
-        return "return getUser was called with userId: " + userId;
+    public User getUser(@PathVariable int userId) {
+
+        User user = new User();
+        user.setFirstname("Sergey");
+        user.setLastname("Blalb");
+
+        return user;
     }
 
     @PostMapping
---
+++ src/main/java/com/app/mobileappws/model/response/User.java
@@ -0,0 +1,40 @@
+package com.app.mobileappws.model.response;
+
+public class User {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String userId;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
